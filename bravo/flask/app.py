83d60fe1eb894aaf205172ecab3851effb92873b
amineboudhia <86231033+amineboudhia@users.noreply.github.com>
2022-12-11 07:57:14 -0500

    Add files via upload

---
+++ bravo/flask/app.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+import pymysql
+import sys
+from insert_values import inserto, get_data
+
+
+template_dir = os.path.abspath('templates')
+
+app = Flask(__name__,template_folder=template_dir)
+
+
+@app.route("/")
+def hello_world():
+    data=get_data()
+
+    return render_template('index.html', data=data)
+
+
+@app.route("/ajout",methods=["POST","GET"])
+def Ajout():
+    if request.method == "POST":
+        name = request.form["nom"]
+        prenom = request.form["prenom"]
+        age = request.form["age"]
+        inserto_new=inserto(name,prenom,age)
+
+        return redirect(url_for('hello_world'))
+    else:
+        return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
